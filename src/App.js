e92b219a645eead6d521917a80602c37f9aef7ca
YYHYUki <68013506+YYHYUki@users.noreply.github.com>
2022-11-22 02:39:32 +0900

    Feat  18ppt-20 for wasd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({name, picture,rating}) {
+  return( 
+  <div>
+  <h1>I like {name}</h1>
+  <h4>{rating}/5.0</h4>
+  <img src = {picture} alt ={name}/>
+  </div>
   );
+
+}
+
+const foodILike= [
+
+  {
+    id : 1,
+    name : 'kimchi',
+    image : 'https://bit.ly/3d14HVx',
+    rating:4.9,
+  },
+  { 
+    id : 2,
+    name : 'Samgyeopsal',
+    image:'http://asq.kr/wttMFjg1Ikyc',
+    rating:5,
+  },
+];
+
+function renderFood(dish){
+  return <Food name = {dish.name} image = {dish.image}/>;
+}
+
+function App(){
+  return(
+    <div>
+   {foodILike.map(dish=>(<Food key={dish.id} name = {dish.name} image = {dish.image}
+   rating = {dish.rating}
+   />))}
+    </div>
+  )
+}
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating: PropTypes.number,
 }
 
 export default App;
